62339146f87f5b858cce8017d8acdcae9a9ced51
marioProfeJOYFE <mario.rios@iepgroup.es>
2026-03-24 18:48:34 +0100

    Interfaz como en Figma

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,24 +1,49 @@
-﻿using System.Text;
+﻿using System;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace JuegosDAM
+namespace JuegosDAM 
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        // Permite mover la ventana al hacer clic sostenido en el fondo
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            // Solo arrastra si se está haciendo clic con el botón izquierdo
+            if (e.ButtonState == MouseButtonState.Pressed)
+            {
+                this.DragMove();
+            }
+        }
+
+        // Acción para el botón ROJO (Cerrar)
+        private void BtnClose_Click(object sender, MouseButtonEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        // Acción para el botón AMARILLO (Minimizar)
+        private void BtnMinimize_Click(object sender, MouseButtonEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        // Acción para el botón VERDE (Maximizar / Restaurar)
+        private void BtnMaximize_Click(object sender, MouseButtonEventArgs e)
+        {
+            if (this.WindowState == WindowState.Normal)
+            {
+                this.WindowState = WindowState.Maximized;
+            }
+            else
+            {
+                this.WindowState = WindowState.Normal;
+            }
         }
     }
 }
